7b72f7807c472e60058a14530618032dea5346b2
Eb3yr <adamkjohnson123@gmail.com>
2024-09-07 22:51:56 +0100

    Defines three symbols to conditionally compile implicit casts to Godot and Monogame Color structs.

---
+++ ColourLib/Definitions.cs
@@ -0,0 +1,23 @@
+﻿#define UNITY
+#define GODOT
+#define MONOGAME
+
+namespace ColourLib
+{
+	partial struct Color : IColorF<Color>
+	{
+		#if UNITY
+
+		#endif
+
+		#if GODOT
+		public static implicit operator Godot.Color(Color color) => new(color.r, color.g, color.b, color.a);
+		public static implicit operator Color(Godot.Color color) => new(color.R, color.G, color.B, color.A);
+		#endif
+
+		#if MONOGAME
+		public static implicit operator Microsoft.Xna.Framework.Color(Color color) => new(color.r, color.g, color.b, color.a);
+		public static implicit operator Color(Microsoft.Xna.Framework.Color color) => new(color.R, color.G, color.B, color.A);
+		#endif
+	}
+}
